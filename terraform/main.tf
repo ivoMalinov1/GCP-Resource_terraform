7cd9ac41e0d6d3fc1a5ed5bc490cd4b32a3c8e25
ivo malinov <ivo.malinov3@gmail.com>
2023-05-16 14:20:15 +0300

    bucket creation

---
+++ terraform/main.tf
@@ -0,0 +1,9 @@
+
+provider "google" {
+  credentials = file("C:/Users/ivoma/Downloads/acoustic-cirrus-378711-369a6bad53ee.json")
+  project     = "acoustic-cirrus-378711"
+  region      = "us-central1"
+}
+
+
+
---
+++ terraform/storage.tf
@@ -0,0 +1,4 @@
+resource "google_storage_bucket" "my_bucket" {
+  name     = "my-bucketivoo1"
+  location = "US"
+}
